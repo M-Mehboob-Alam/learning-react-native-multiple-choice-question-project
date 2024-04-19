585e71359d18203f3ba88c72249dcd8928af03f2
Mehboob Alam <mehboobalam.dev@gmail.com>
2024-04-19 12:52:04 +0500

    create custom component inside app component

--- App.js
+++ App.js
@@ -3,38 +3,26 @@ import { Image, StyleSheet, Text, View } from 'react-native';
 
 import styles from './App.styles';
 export default function App() {
+  const ImageOption = () => {
+    return (
+      <View style={styles.optionContainer}>
+      <Image source={{uri: 'https://e7.pngegg.com/pngimages/406/1017/png-clipart-classical-chinese-tea-cup-chinese-style-tea-cup-thumbnail.png'}}
+        style={styles.image} 
+        resizeMode='contain'
+        />
+        <Text style={styles.optionText}>Cup of tea</Text>
+      </View> 
+    )
+  }
   return (
     <View style={styles.root}>
       <Text style={styles.question}>Which one is a cup of tea?</Text>    
       <View style={styles.optionsContainer}>
-        <View style={styles.optionContainer}>
-        <Image source={{uri: 'https://e7.pngegg.com/pngimages/406/1017/png-clipart-classical-chinese-tea-cup-chinese-style-tea-cup-thumbnail.png'}}
-          style={styles.image} 
-          resizeMode='contain'
-          />
-          <Text style={styles.optionText}>Cup of tea</Text>
-        </View>  
-        <View style={styles.optionContainer}>
-        <Image source={{uri: 'https://e7.pngegg.com/pngimages/406/1017/png-clipart-classical-chinese-tea-cup-chinese-style-tea-cup-thumbnail.png'}}
-          style={styles.image} 
-          resizeMode='contain'
-          />
-          <Text style={styles.optionText}>Cup of tea</Text>
-        </View>  
-        <View style={styles.optionContainer}>
-        <Image source={{uri: 'https://e7.pngegg.com/pngimages/406/1017/png-clipart-classical-chinese-tea-cup-chinese-style-tea-cup-thumbnail.png'}}
-          style={styles.image} 
-          resizeMode='contain'
-          />
-          <Text style={styles.optionText}>Cup of tea</Text>
-        </View>  
-        <View style={styles.optionContainer}>
-        <Image source={{uri: 'https://e7.pngegg.com/pngimages/406/1017/png-clipart-classical-chinese-tea-cup-chinese-style-tea-cup-thumbnail.png'}}
-          style={styles.image} 
-          resizeMode='contain'
-          />
-          <Text style={styles.optionText}>Cup of tea</Text>
-        </View>  
+      <ImageOption />
+      <ImageOption />
+      <ImageOption />
+      <ImageOption />
+       
       </View>  
      
     </View>
